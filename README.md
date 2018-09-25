81c0a7fc83ec391f8be3094207e8d07a60ab7ead
Cornelis Heuperman <cheuperman@gmail.com>
2018-09-25 14:27:12 +0100

    Allow user to change colours of brush

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # web tag
-A Chrome extension that lets you draw on top of websites using the J5 JavaScript library.
+A Chrome extension that lets you draw on top of websites using the p5 JavaScript library.
 
 ### How to use:
 - open Chrome
 - visit [chrome://extensions](chrome://extensions)
 - click 'Load unpacked'
 - select the web tag folder
-- visit a webpage
+- visit any webpage
 - press the web tag button on the top right of your browser
 
 You will now be able to draw on the web page. Enjoy!
 
 ## List of commands:
 While web tag is running press:  
-`[` -- decrease the brush size  
-`]` -- increase the brush size  
-
-## Coming soon:
-Multiple colours!
+`[` -- to decrease the brush size  
+`]` -- to increase the brush size  
+The number keys to change colours
 
 ## Credit:
 For the all the hard work: [the p5.js team](https://p5js.org/)  
--- content.js
+++ content.js
@@ -5,16 +5,52 @@ chrome.runtime.onMessage.addListener(
   });
 
 let weight = 13;
+let rgbValues = [0, 0, 0];
 let fired = false;
 
 document.onkeydown = (event) => {
-  if (event.code == 'BracketRight' && !fired) {
-    weight += 2;
-  } else if (event.code == 'BracketLeft') {
-    weight -= 2;
+  if (!fired) {
+    switch (event.code) {
+      case 'BracketRight':
+        weight += 2;
+        break;
+      case 'BracketLeft':
+        weight -= 2;
+        break;
+      case 'Digit1':
+        rgbValues = [244, 67, 54];
+        break;
+      case 'Digit2':
+        rgbValues = [255, 152, 0];
+        break;
+      case 'Digit3':
+        rgbValues = [255, 235, 59];
+        break;
+      case 'Digit4':
+        rgbValues = [76, 175, 80];
+        break;
+      case 'Digit5':
+        rgbValues = [0, 188, 212];
+        break;
+      case 'Digit6':
+        rgbValues = [33, 150, 243];
+        break;
+      case 'Digit7':
+        rgbValues = [63, 81, 181];
+        break;
+      case 'Digit8':
+        rgbValues = [156, 39, 176];
+        break;
+      case 'Digit9':
+        rgbValues = [0, 0, 0];
+        break;
+      case 'Digit0':
+        rgbValues = [255, 255, 255];
+        break;
+    }
+    fired = true;
   }
-  fired = true;
-};
+}
 
 document.onkeyup = (event) => {
   fired = false;
@@ -34,7 +70,7 @@ let tag = function(tag) {
   }
 
   tag.draw = () => {
-    tag.stroke(255, 204, 0);
+    tag.stroke(rgbValues[0], rgbValues[1], rgbValues[2]);
     tag.strokeWeight(weight);
     if(tag.mouseIsPressed){
       tag.line(tag.mouseX, tag.mouseY, tag.pmouseX, tag.pmouseY)
